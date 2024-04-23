ca1544c99294ccf26dc5ea4940dbd61d59f5281f
Asadz2601 <113413370+Asadz2601@users.noreply.github.com>
2024-04-23 20:18:52 +0500

    FYP

--- Adminpanel/side/index.php
+++ Adminpanel/side/index.php
@@ -59,7 +59,7 @@ $data = [
 
 // Convert data to JSON format!!!!
 $data_json = json_encode($data);
-    ?>
+?>
 
 
 <br><br><br><br><br>
@@ -73,98 +73,40 @@ $data_json = json_encode($data);
                 <div class="col-md-3" style="margin-bottom: 10px;">
                     <div class="card bg-primary " style="width: 14rem;">
                         <div class="card-body">
-                          <h2 class="card-title font-weight-bolder text-light" style="font-weight:bolder;font-size:large;"><?php echo $count; ?></h2>
-                          <h6 class="card-subtitle mb-2  text-light">Users</h6>
-                          <!-- <i class="bi bi-bar-chart" style="font-size: 5rem ; width: 5rem; height: 5rem ;"></i> -->
+                            <h2 class="card-title font-weight-bolder text-light" style="font-weight:bolder;font-size:large;"><?php echo $count; ?></h2>
+                            <h6 class="card-subtitle mb-2  text-light">Users</h6>
+                            <!-- <i class="bi bi-bar-chart" style="font-size: 5rem ; width: 5rem; height: 5rem ;"></i> -->
                         </div>
                     </div>
                 </div>
                 <div class="col-md-3" style="margin-bottom: 10px;">
                     <div class="card bg-warning " style="width: 14rem;">
                         <div class="card-body">
-                          <h2 class="card-title font-weight-bolder text-light" style="font-weight:bolder;font-size:large;"><?php echo $count1; ?></h2>
-                          <h6 class="card-subtitle mb-2  text-light">Orders</h6>
+                            <h2 class="card-title font-weight-bolder text-light" style="font-weight:bolder;font-size:large;"><?php echo $count1; ?></h2>
+                            <h6 class="card-subtitle mb-2  text-light">Orders</h6>
                         </div>
                     </div>
                 </div>
                 <div class="col-md-3" style="margin-bottom: 10px;">
-                     <div class="card bg-success " style="width: 14rem;">
+                    <div class="card bg-success " style="width: 14rem;">
                         <div class="card-body">
-                          <h2 class="card-title font-weight-bolder text-light" style="font-weight:bolder;font-size:large;"><?php echo $count2; ?></h2>
-                          <h6 class="card-subtitle mb-2  text-light">Admins</h6>
+                            <h2 class="card-title font-weight-bolder text-light" style="font-weight:bolder;font-size:large;"><?php echo $count2; ?></h2>
+                            <h6 class="card-subtitle mb-2  text-light">Admins</h6>
                         </div>
                     </div>
                 </div>
                 <div class="col-md-3" style="margin-bottom: 10px;">
                     <div class="card bg-danger " style="width: 14rem;">
                         <div class="card-body">
-                          <h2 class="card-title font-weight-bolder text-light" style="font-weight:bolder;font-size:large;"><?php echo $count3; ?></h2>
-                          <h6 class="card-Subtitle mb-2  text-light">Products</h6>
+                            <h2 class="card-title font-weight-bolder text-light" style="font-weight:bolder;font-size:large;"><?php echo $count3; ?></h2>
+                            <h6 class="card-Subtitle mb-2  text-light">Products</h6>
                         </div>
                     </div>
                 </div>
                 <div style="width: 50%; border: 1.5px solid #ccc; box-shadow: 0px 0px 10px rgba(0, 0, 0, 0.1);">
-        <canvas id="myChart"></canvas>
-    </div>
+                    <canvas id="myChart"></canvas>
+                </div>
             </div>
         </div>
     </div>
 </div>
-
-
-
-
-
-
-
-
-
-
-
-
-    <script>
-        // Parse data from PHP to JavaScript
-        var data = <?php echo $data_json; ?>;
-        var labels = Object.keys(data);
-        var values = Object.values(data);
-
-        // Create pie chart
-        var ctx = document.getElementById('myChart').getContext('2d');
-        var myChart = new Chart(ctx, {
-            type: 'pie',
-            data: {
-                labels: labels,
-                datasets: [{
-                    data: values,
-                    backgroundColor: [
-                        'rgba(255, 99, 132, 0.5)',
-                        'rgba(54, 162, 235, 0.5)',
-                        'rgba(255, 206, 86, 0.5)',
-                        'rgba(75, 192, 192, 0.5)',
-                    ],
-                    borderColor: [
-                        'rgba(255, 99, 132, 1)',
-                        'rgba(54, 162, 235, 1)',
-                        'rgba(255, 206, 86, 1)',
-                        'rgba(75, 192, 192, 1)',
-                    ],
-                    borderWidth: 1
-                }]
-            },
-            options: {
-                responsive: true,
-                maintainAspectRatio: false,
-                legend: {
-                    display: true,
-                    position: 'bottom'
-                },
-                title: {
-                    display: true,
-                    text: 'Data Distribution'
-                }
-            }
-        });
-    </script>
-<?php
-  include('footer.php');
-?>
